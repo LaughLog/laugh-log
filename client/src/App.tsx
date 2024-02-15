91bc9ae3752d798fd9afadc4350ac239a311a920
ovoxiix <ovoxiix@gmail.com>
2024-02-15 21:46:56 +0900

    feat: Canvas 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from '@emotion/react';
 
 import theme from '@styles/theme';
 import GlobalStyles from '@styles/GlobalStyles';
+import Canvas from '@components/Canvas/Canvas';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Canvas />
     </ThemeProvider>
   );
 }
---
+++ client/src/components/Canvas/Canvas.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Canvas = () => {
+  return <Container />;
+};
+
+export default Canvas;
+
+const Container = styled.canvas`
+  width: 100vw;
+  height: 100vh;
+`;
