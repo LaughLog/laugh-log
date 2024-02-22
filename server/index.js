be2c63d4cb9404457e8a162ea480f2a7bdbf7111
JitHoon <imessi0336@gmail.com>
2024-02-22 22:38:33 +0900

    [server] test: EC2 탄력적 ip 배포 GET 테스트

--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ const app = express();
 
 const server = http.createServer(app);
 
+app.get("/", (_req, res) => {
+  res.send(`<html>
+                  <body>
+                      <h1 style="color:blue;text-align: center;margin-top: 100px;">EC2 배포 성공</h1>
+                      <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                          <img src="https://picsum.photos/400/400?random=1">
+                      </div>
+                  </body>
+                 </html>`);
+});
+
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -29,4 +40,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(8080, () => console.log("Listening to port 8080"));
+server.listen(5001, () => console.log("Listening to port 5001"));
