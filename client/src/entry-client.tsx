6cb7c08224afeda6933d14d58c99d2c51bb9f568
ovoxiix <ovoxiix@gmail.com>
2024-02-22 15:32:29 +0900

    fix: 빌드 에러 수정

--- client/src/entry-client.tsx
+++ client/src/entry-client.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from '@styles/GlobalStyles';
 import { ThemeProvider } from '@emotion/react';
-import { theme } from '@styles/theme';
+import theme from '@styles/theme';
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
