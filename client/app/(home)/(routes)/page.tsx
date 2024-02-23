812a8604b3bddf9cc9117853d917b6b21974d2d9
Bumang-Cyber <jove0729@naver.com>
2024-02-23 10:42:04 +0900

    fix: 예시 텍스트 수정

--- client/app/(home)/(routes)/page.tsx
+++ client/app/(home)/(routes)/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <div className="text-coral800">왜 안 돼?</div>
-      <span className="text-subtitle1 text-coral800">왜 안 돼</span>
-      <div className="text-subtitle3">되는거야?</div>
+      <div className="text-coral800">적용1</div>
+      <span className="text-subtitle1 text-coral800">적용2</span>
+      <div className="text-subtitle3">적용3</div>
     </main>
   );
 }
