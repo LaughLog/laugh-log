fd0c57c7452a7a33246c20a88276c65703ffa90b
Bumang-Cyber <jove0729@naver.com>
2024-02-24 00:36:50 +0900

    feat: shadcn/ui 초기 설정 및 버튼 컴포넌트 색상 지정

--- client/app/(home)/(routes)/page.tsx
+++ client/app/(home)/(routes)/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -6,6 +6,11 @@ export default function Home() {
       <div className="text-coral800">적용1</div>
       <span className="subtitle1 w-full text-coral800">수정된 텍스트</span>
       <div className="text-subtitle3">적용3</div>
+      <Button>프라이머리</Button>
+      <Button variant="secondary">세컨더리</Button>
+      <Button variant="sidebar" className="w-[100%]">
+        초대
+      </Button>
     </main>
   );
 }
