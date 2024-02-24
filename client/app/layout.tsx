407a85cdf8b44850e6f47c31f5a50f6eca160753
JitHoon <imessi0336@gmail.com>
2024-02-24 23:19:10 +0900

    [client] feat: root layout에 clerk provider 세팅

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Laugh Log : 웃음으로 가득한 회의록',
-  description: '회의는 단순한 업무가 아닙니다. 회의는 즐거운 협업의 시작입니다. Laugh Log으로 회의를 더욱 생생하게 기록하세요!'
+  description:
+    '회의는 단순한 업무가 아닙니다. 회의는 즐거운 협업의 시작입니다. Laugh Log으로 회의를 더욱 생생하게 기록하세요!'
 };
 
 export default function RootLayout({
@@ -12,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="ko">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
