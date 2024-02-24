1f9b8a91940e784a49b8ca160f28bb4a68c274e9
ovoxiix <ovoxiix@gmail.com>
2024-02-24 23:19:11 +0900

    [client] chore: clerk 설치 및 초기 세팅

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,11 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Laugh Log : 웃음으로 가득한 회의록',
-  description: '회의는 단순한 업무가 아닙니다. 회의는 즐거운 협업의 시작입니다. Laugh Log으로 회의를 더욱 생생하게 기록하세요!'
+  description:
+    '회의는 단순한 업무가 아닙니다. 회의는 즐거운 협업의 시작입니다. Laugh Log으로 회의를 더욱 생생하게 기록하세요!'
 };
 
 export default function RootLayout({
@@ -13,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
     </html>
   );
 }
