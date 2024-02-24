25bc7d16b758470f19df1a05a13bdcff23c5b93e
JitHoon <imessi0336@gmail.com>
2024-02-24 23:23:48 +0900

    [client] design: /sign-in 페이지 마크업

--- client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
 
-const SignIn = () => {
-  return <div>SignIn</div>;
+import './sign-in.css';
+
+// FIX: 접속 시 로딩 및 로그인 버튼 클릭 이후 로딩 처리 필요
+const Page = () => {
+  return (
+    <div className="bg-signIn flex h-screen w-screen items-center justify-evenly bg-cover">
+      <div className="mx-[20px] w-[270px] flex-col flex-wrap text-center">
+        <span className="text-[35px] font-bold">
+          어서오세요
+          <br />
+          Laugh Log에
+          <br />
+          접속합니다.
+        </span>
+        <SignIn />
+      </div>
+      <Image src="/sign-in-welcome.svg" alt="Logo" height={600} width={720} />
+    </div>
+  );
 };
 
-export default SignIn;
+export default Page;
