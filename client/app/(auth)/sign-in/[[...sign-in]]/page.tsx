4f20a336ac773244258a85477d3ab147e9eb43b6
Bumang-Cyber <jove0729@naver.com>
2024-02-28 03:05:13 +0900

    fix: clerk 주석처리 해제

--- client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,4 @@
-// import { SignIn } from '@clerk/nextjs';
+import { SignIn } from '@clerk/nextjs';
 import Image from 'next/image';
 
 import './sign-in.css';
@@ -6,7 +6,7 @@ import './sign-in.css';
 // FIX: 접속 시 로딩 및 로그인 버튼 클릭 이후 로딩 처리 필요
 const Page = () => {
   return (
-    <div className="bg-signIn flex h-screen w-screen items-center justify-evenly bg-cover">
+    <div className="flex h-screen w-screen items-center justify-evenly bg-signIn bg-cover">
       <div className="mx-[20px] w-[270px] flex-col flex-wrap text-center">
         <span className="text-[35px] font-bold">
           어서오세요
@@ -15,7 +15,7 @@ const Page = () => {
           <br />
           접속합니다.
         </span>
-        {/* <SignIn /> */}
+        <SignIn />
       </div>
       <Image src="/sign-in-welcome.svg" alt="Logo" height={600} width={720} />
     </div>
