5850f8284f876356a35a48dae2266ebe85e938e7
JitHoon <imessi0336@gmail.com>
2024-03-01 16:40:36 +0900

    rename: 로그인 파일명 컨벤션 적용

--- client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,12 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignIn as SignInClerk } from '@clerk/nextjs';
 import Image from 'next/image';
 
 import './sign-in.css';
 
 // FIX: 접속 시 로딩 및 로그인 버튼 클릭 이후 로딩 처리 필요
-const Page = () => {
+const SignIn = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-evenly bg-signIn bg-cover">
+    <div className="bg-signIn flex h-screen w-screen items-center justify-evenly bg-cover">
       <div className="mx-[20px] w-[270px] flex-col flex-wrap text-center">
         <span className="text-[35px] font-bold">
           어서오세요
@@ -15,11 +15,11 @@ const Page = () => {
           <br />
           접속합니다.
         </span>
-        <SignIn />
+        <SignInClerk />
       </div>
       <Image src="/sign-in-welcome.svg" alt="Logo" height={600} width={720} />
     </div>
   );
 };
 
-export default Page;
+export default SignIn;
