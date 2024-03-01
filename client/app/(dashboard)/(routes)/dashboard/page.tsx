9a3969888175538b944cb3c7470bae16c2a52883
ovoxiix <ovoxiix@gmail.com>
2024-03-01 22:15:13 +0900

    [client] design: 대시보드에 `템플릿으로 시작하기` 추가

--- client/app/(dashboard)/(routes)/dashboard/page.tsx
+++ client/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import MainContainer from '@/app/(dashboard)/_components/mainContainer';
+import StartTemplate from '@/app/(dashboard)/_components/startTemplate';
+import TeamBoard from '@/app/(dashboard)/_components/teamBoard';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <MainContainer>
+      <StartTemplate />
+      <TeamBoard />
+    </MainContainer>
+  );
 };
 
 export default Dashboard;
