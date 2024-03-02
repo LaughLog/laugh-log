72a50fc572c96cc8b053ace41506ca851862f074
ovoxiix <ovoxiix@gmail.com>
2024-03-02 16:29:20 +0900

    [client] design: 대시보드 레이아웃 수정

--- client/app/(dashboard)/(routes)/dashboard/page.tsx
+++ client/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,16 @@
-import MainContainer from '@/app/(dashboard)/_components/mainContainer';
-import StartTemplate from '@/app/(dashboard)/_components/startTemplate';
+import Sidebar from '@/app/(dashboard)/_components/sidebar/sidebar';
+import StartWithTemplate from '@/app/(dashboard)/_components/startWithTemplate';
 import TeamBoard from '@/app/(dashboard)/_components/teamBoard';
 
 const Dashboard = () => {
   return (
-    <MainContainer>
-      <StartTemplate />
-      <TeamBoard />
-    </MainContainer>
+    <div className="flex h-full w-full">
+      <Sidebar />
+      <div className="flex h-full w-[calc(100vw-328px)] flex-col justify-between gap-16 overflow-hidden px-14">
+        <StartWithTemplate />
+        <TeamBoard />
+      </div>
+    </div>
   );
 };
 
--- client/app/(dashboard)/_components/teamBoard.tsx
+++ client/app/(dashboard)/_components/teamBoard.tsx
@@ -1,5 +1,5 @@
 const TeamBoard = () => {
-  return <div className="h-[calc(100%-272px)] w-full bg-coral400">팀보드</div>;
+  return <div className="h-[calc(100vh-332px)] w-full bg-coral400">팀보드</div>;
 };
 
 export default TeamBoard;
