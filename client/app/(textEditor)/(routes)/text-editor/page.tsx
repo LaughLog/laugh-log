88fe2abab2bd5fcd17e7be45987b0e9141566194
JitHoon <imessi0336@gmail.com>
2024-03-19 20:22:11 +0900

    [client] feat: /text-editor 라우터 페이지 생성

---
+++ client/app/(textEditor)/(routes)/text-editor/page.tsx
@@ -0,0 +1,19 @@
+import { Block } from '@/types/textEditor';
+import { uid } from '@/lib/utils';
+import EditablePage from '../../_components/editablePage';
+
+const TextEditor = () => {
+  // FIX : 향후 db에서 reactQuery를 활용하여 initialBlocks 불러옵니다.
+  const initialBlocks: Block[] = [
+    { id: uid(), html: '1', tag: 'h1' },
+    { id: uid(), html: '2', tag: 'p' }
+  ];
+
+  return (
+    <section className="mx-auto my-0 w-2/3">
+      <EditablePage initialBlocks={initialBlocks} />
+    </section>
+  );
+};
+
+export default TextEditor;
