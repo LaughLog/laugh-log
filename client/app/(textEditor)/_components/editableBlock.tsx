90d16f872613d14e71f9059ec6c8b439a7c8cc53
최지훈 <101972330+JitHoon@users.noreply.github.com>
2024-04-25 12:57:02 +0900

    [#44] 키보드 상하 화살표 입력으로 block 위치 이동하기 (#48)
    * [client] feat: '/' 입력 시 메뉴 오픈
    
    * [client] modify: 사용하지않는 상태 및 주석 삭제
    
    * [client] modify: 불필요한 타입 import 제거
    
    * [client] modify: welcome 문구 불필요한 리렌더링 방지
    
    * [client] feat: menu open시 사용자 좌표 설정 및 menu close
    
    * [client] modify: menu가 열려있을 때만 menu open
    
    * [client] feat: menu 목록 UI 생성
    
    * [client] feat: 선택 menu에 따라 tag 변경
    
    * [client] feat: '/' command 위치에 따른 메뉴 위치 지정 및 스타일 적용
    
    * [client] remove: 사용하지 않는 match-sorter 라이브러리 삭제
    
    * [client] feat: block 삭제 시 사용자 커서 끝으로 위치 시키기
    
    * [client] feat: 키보드 ArrowUp 입력시 위 block으로 이동하기
    
    * [client] feat: 키보드 ArrowDown 입력시 아래 block으로 이동하기
    
    * [client] fix: 함수 중복 선언 제거

--- client/app/(textEditor)/_components/editableBlock.tsx
+++ client/app/(textEditor)/_components/editableBlock.tsx
@@ -35,10 +35,13 @@ const EditableBlock = ({
 
   // 키 다운 이벤트 핸들러
   const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    // 한글 key일 때 2번 입력되는 오류 방지
+    if (e.nativeEvent.isComposing) return;
+
     // 새로운 블록 추가
     if (e.key === 'Enter') {
-      // 한글 key일 때 2번 입력되는 오류 방지
-      if (e.nativeEvent.isComposing) return;
+      // 새로운 박스 생성 전 menu close
+      setMenuIsOpen(false);
 
       // 새로운 박스 생성 전 menu close
       setMenuIsOpen(false);
@@ -65,6 +68,56 @@ const EditableBlock = ({
       // article 태그를 기준으로 가장 첫 번째 block 유지
       if (previousBlock && previousBlock.tagName !== 'ARTICLE') {
         deleteBlock({ id: block.id, previousBlock });
+      }
+    }
+
+    // 블록 이동
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+
+      const currentBlock = contentEditable.current;
+      const previousBlock = currentBlock?.previousElementSibling as
+        | HTMLInputElement
+        | undefined;
+
+      // 이전 블록이 존재하고, article 태그가 아닌 경우에만 이동
+      if (previousBlock && previousBlock.tagName !== 'ARTICLE') {
+        const range = document.createRange();
+        const selection = window.getSelection();
+
+        // 기존 Selection의 range 삭제
+        selection?.removeAllRanges();
+
+        // 새로운 Range 객체에 대한 커서 위로 이동 후 새로운 range 추가
+        range.selectNodeContents(previousBlock);
+        range.collapse(false);
+        selection?.addRange(range);
+
+        previousBlock.focus();
+      }
+    }
+
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+
+      const currentBlock = contentEditable.current;
+      const nextBlock = currentBlock?.nextElementSibling as
+        | HTMLInputElement
+        | undefined;
+
+      if (nextBlock) {
+        const range = document.createRange();
+        const selection = window.getSelection();
+
+        // 기존 Selection의 range 삭제
+        selection?.removeAllRanges();
+
+        // 새로운 Range 객체에 대한 커서 아래로 이동 후 새로운 range 추가
+        range.selectNodeContents(nextBlock);
+        range.collapse(false);
+        selection?.addRange(range);
+
+        nextBlock.focus();
       }
     }
   };
