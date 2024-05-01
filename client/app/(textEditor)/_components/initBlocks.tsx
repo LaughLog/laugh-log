b6b06a387c64609abd26a6add59029ac651424f1
최지훈 <101972330+JitHoon@users.noreply.github.com>
2024-05-01 14:36:58 +0900

    [#59] 보드 id로 텍스트 에디터 db 검색 (#60)
    * [server] feat: block 추가, 삭제, content 및 tag 변경 socket 이벤트 등록
    
    * [client] feat: 텍스트 에디터 초기 socket 연결
    
    * [client] feat: firebase block 불러오기
    
    * [client] modify: socket 연결로 인한 불필요한 코드 삭제
    
    * [client] feat: socket 이벤트 block 삭제, 추가, content 및 tag 변경 구현
    
    * [client] feat: menu 선택시 tag 변경 socket event emit
    
    * [client] 텍스트 에디터 상수 및 타입 정리
    
    * [client] feat: 디바운스 적용하여 정확한 content값 전달하기
    
    * [client] feat: 실제 편집자의 화면은 content 바로 반영하기
    
    * [server] modify: 사용하지 않는 socket event 삭제
    
    * [client] modify: 불필요한 주석 삭제
    
    * [client] feat: 쿼리 스트링으로 텍스트 에디터 db 검색하기

--- client/app/(textEditor)/_components/initBlocks.tsx
+++ client/app/(textEditor)/_components/initBlocks.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { doc } from 'firebase/firestore';
+import { useSearchParams } from 'next/navigation';
 
 import { db } from '@/firebase/app';
 import EditablePage from './editablePage';
@@ -8,12 +9,13 @@ import useGetBlocks from '@/hook/queries/useGetBlocks';
 import { Block } from '@/types/textEditor';
 
 const InitBlocks = () => {
-  const boardId = 'test1'; // 향후 query string으로 대체
-  const textEditorRef = doc(db, 'text-editor', boardId);
+  const params = useSearchParams();
+  const boardId = params.get('id');
+  const textEditorRef = doc(db, 'text-editor', boardId!);
   const { data } = useGetBlocks(textEditorRef);
   const initialBlocks: Block[] = data.data()?.updatedBlocks;
 
-  return <EditablePage initialBlocks={initialBlocks} boardId={boardId} />;
+  return <EditablePage initialBlocks={initialBlocks} boardId={boardId!} />;
 };
 
 export default InitBlocks;
