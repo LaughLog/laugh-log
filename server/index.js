df2912ee6f776f63a9c1483b298620a7285fdda6
Jithoon <imessi0336@gmail.com>
2024-05-01 15:28:19 +0900

    [server] feat: server 배포 restart 테스트

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const server = http.createServer(app);
 app.get("/", (_req, res) => {
   res.send(`<html>
                   <body>
-                      <h1 style="color:blue;text-align: center;margin-top: 100px;">EC2 배포 성공</h1>
                       <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                           <img src="https://picsum.photos/400/400?random=1">
                       </div>
