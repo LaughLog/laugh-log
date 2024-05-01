8d1e52cb672e60a2b1da77bd2d2dcbc3eecccaed
최지훈 <101972330+JitHoon@users.noreply.github.com>
2024-05-01 14:40:17 +0900

    [#61] 로그인 페이지 렌더링 화면 깨짐 개선 및 디자인 변경 (#62)
    * [server] feat: block 추가, 삭제, content 및 tag 변경 socket 이벤트 등록
    
    * [client] feat: 텍스트 에디터 초기 socket 연결
    
    * [client] feat: firebase block 불러오기
    
    * [client] modify: socket 연결로 인한 불필요한 코드 삭제
    
    * [client] feat: socket 이벤트 block 삭제, 추가, content 및 tag 변경 구현
    
    * [client] feat: menu 선택시 tag 변경 socket event emit
    
    * [client] 텍스트 에디터 상수 및 타입 정리
    
    * [client] feat: 디바운스 적용하여 정확한 content값 전달하기
    
    * [client] feat: 실제 편집자의 화면은 content 바로 반영하기
    
    * [server] modify: 사용하지 않는 socket event 삭제
    
    * [client] modify: 불필요한 주석 삭제
    
    * [client] feat: 쿼리 스트링으로 텍스트 에디터 db 검색하기
    
    * [client] modify: 로그인 페이지 렌더링 개선
    
    * [client] modify: 불필요한 주석 삭제

--- client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ client/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,12 @@
 import { SignIn as SignInClerk } from '@clerk/nextjs';
 import Image from 'next/image';
+import SignInBg from '/public/sign-in-welcome.svg';
 
 import './sign-in.css';
 
-// FIX: 접속 시 로딩 및 로그인 버튼 클릭 이후 로딩 처리 필요
 const SignIn = () => {
   return (
-    <div className="bg-signIn flex h-screen w-screen items-center justify-evenly bg-cover">
+    <div className="flex h-screen w-screen items-center justify-evenly bg-signIn bg-cover">
       <div className="mx-[20px] w-[270px] flex-col flex-wrap text-center">
         <span className="text-[35px] font-bold">
           어서오세요
@@ -15,9 +15,11 @@ const SignIn = () => {
           <br />
           접속합니다.
         </span>
-        <SignInClerk />
+        <div className="h-[100px]">
+          <SignInClerk />
+        </div>
       </div>
-      <Image src="/sign-in-welcome.svg" alt="Logo" height={600} width={720} />
+      <Image src={SignInBg} alt="Sign In Background" height={600} width={720} />
     </div>
   );
 };
