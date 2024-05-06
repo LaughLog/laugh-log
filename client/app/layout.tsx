dc88a941fc2276648193e236b8cc593b28530494
Jithoon <imessi0336@gmail.com>
2024-05-06 19:55:17 +0900

    [client] feat: favicon 및 메타 데이터 추가

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,7 +7,19 @@ import ReactQueryProvider from '@/provider/react-query-provider';
 export const metadata: Metadata = {
   title: 'Laugh Log : 웃음으로 가득한 회의록',
   description:
-    '회의는 단순한 업무가 아닙니다. 회의는 즐거운 협업의 시작입니다. Laugh Log으로 회의를 더욱 생생하게 기록하세요!'
+    '회의는 단순한 업무가 아닙니다. 회의는 즐거운 협업의 시작입니다. Laugh Log으로 회의를 더욱 생생하게 기록하세요!',
+  keywords: '회의, 텍스트에디터, 노션, 기록, 회의록',
+  openGraph: {
+    title: 'Laugh Log : 웃음으로 가득한 회의록',
+    description: 'Laugh Log으로 회의를 더욱 생생하게 기록하세요!',
+    images: '/public/tomato-sticker.svg',
+    type: 'website'
+  },
+  twitter: {
+    title: 'Laugh Log : 웃음으로 가득한 회의록',
+    description: 'Laugh Log으로 회의를 더욱 생생하게 기록하세요!',
+    card: 'summary'
+  }
 };
 
 export default function RootLayout({
@@ -16,12 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="ko">
-        <body>
+    <html lang="ko">
+      <body>
+        <ClerkProvider>
           <ReactQueryProvider>{children}</ReactQueryProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
